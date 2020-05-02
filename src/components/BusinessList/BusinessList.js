35d27da79cd36e97a83e981f71cbb1f0d5337489
Jean <nguyen.jean@me.com>
2020-05-02 23:36:23 +0200

    add  <BusinessList />

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './BusinessList.css';
+import Bussines from '../Business/Business';
+
+
+class BusinessList extends React.component{
+    render(
+        return (
+            <div class='BusinessList'>
+            <!-- Add Business Component here --> 
+            <!-- Add Business Component here -->
+            <!-- Add Business Component here -->
+            <!-- Add Business Component here -->
+            <!-- Add Business Component here -->
+            <!-- Add Business Component here -->
+            </div>
+        )
+    )
+}
