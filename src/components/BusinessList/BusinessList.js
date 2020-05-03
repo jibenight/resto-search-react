150a9e6d93d6a574d4f0c6916e61eeafaf3b1539
Jean <nguyen.jb@free.fr>
2020-05-03 21:29:22 +0200

    correct class

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './BusinessList.css';
-import Bussines from '../Business/Business';
+import Business from '../Business/Business';
 
-
-class BusinessList extends React.component{
-    render(
-        return (
-            <div class='BusinessList'>
-            <!-- Add Business Component here --> 
-            <!-- Add Business Component here -->
-            <!-- Add Business Component here -->
-            <!-- Add Business Component here -->
-            <!-- Add Business Component here -->
-            <!-- Add Business Component here -->
-            </div>
-        )
-    )
+class BusinessList extends React.Component {
+  render() {
+    return (
+      <div class="BusinessList">
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+      </div>
+    );
+  }
 }
+
+export default BusinessList;
