3e1110f2eb8d7d4921c73694f2c1f1516ac65205
Jean <nguyen.jean@me.com>
2020-05-06 13:36:27 +0200

    finish SearchBar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -9,8 +9,26 @@ const sortByOptions = {
 
 class SearchBar extends React.Component {
   renderSortByOptions() {
-    return Object.keys(sortByOptions).map((sortByOptions) => {
-      let sortByOptionValue = sortByOptions[sortByOptions];
+    return Object.keys(sortByOptions).map((sortByOption) => {
+      let sortByOptionValue = sortByOptions[sortByOption];
+      <li key={sortByOptionValue}>{sortByOption}</li>;
     });
   }
+
+  render() {
+    <div className="SearchBar">
+      <div className="SearchBar-sort-options">
+        <ul>{renderSortByOptions()}</ul>
+      </div>
+      <div className="SearchBar-fields">
+        <input placeholder="Search Businesses" />
+        <input placeholder="Where?" />
+      </div>
+      <div className="SearchBar-submit">
+        <a>Let's Go</a>
+      </div>
+    </div>;
+  }
 }
+
+export default SearchBar;
