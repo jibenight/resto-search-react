634dbfad8f7149c4325e9f88dbf4b54d5281e83b
Jean <nguyen.jean@me.com>
2020-05-06 15:44:44 +0200

    Update the render  of the class APP

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './searchBar.css';
+import './SearchBar.css';
 
 const sortByOptions = {
   'Best Match': 'best_match',
@@ -11,23 +11,25 @@ class SearchBar extends React.Component {
   renderSortByOptions() {
     return Object.keys(sortByOptions).map((sortByOption) => {
       let sortByOptionValue = sortByOptions[sortByOption];
-      <li key={sortByOptionValue}>{sortByOption}</li>;
+      return <li key={sortByOptionValue}>{sortByOption}</li>;
     });
   }
 
   render() {
-    <div className="SearchBar">
-      <div className="SearchBar-sort-options">
-        <ul>{renderSortByOptions()}</ul>
+    return (
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>{this.renderSortByOptions()}</ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+          <a>Let's Go</a>
+        </div>
       </div>
-      <div className="SearchBar-fields">
-        <input placeholder="Search Businesses" />
-        <input placeholder="Where?" />
-      </div>
-      <div className="SearchBar-submit">
-        <a>Let's Go</a>
-      </div>
-    </div>;
+    );
   }
 }
 
