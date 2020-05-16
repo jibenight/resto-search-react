8231c547750e99b08d1c1436f1205ec9b5dcefe9
Jean <nguyen.jean@me.com>
2020-05-16 22:13:21 +0200

    update the render method in app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -56,7 +56,7 @@ class App extends React.Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={this.Business} />
       </div>
     );
   }
