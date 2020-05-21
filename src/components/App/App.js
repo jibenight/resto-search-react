284af8acc4605913889c494a4def48787965cab1
Jean <nguyen.jean@me.com>
2020-05-21 14:28:17 +0200

    add function to add calss=active

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,39 +16,7 @@ const business = {
   reviewCount: 90,
 };
 
-const businesses = [
-  businness,
-  businness1,
-  businness2,
-  businness3,
-  businness4,
-  businness5,
-];
-
-class Business extends React.Component {
-  render() {
-    return (
-      <div className="Business">
-        <div className="image-container">
-          <img src={business.imageSrc} alt="" />
-        </div>
-        <h2>{business.name}</h2>
-        <div className="Business-information">
-          <div className="Business-address">
-            <p>{business.address}</p>
-            <p>{business.city}</p>
-            <p>{`${business.state} ${business.zipCode}`}</p>
-          </div>
-          <div className="Business-reviews">
-            <h3>{business.category.toUpperCase()}</h3>
-            <h3 className="rating">{`${business.rating} stars`}</h3>
-            <p>{`${business.reviewCount} reviews`}</p>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+const businesses = [business, business, business, business, business, business];
 
 class App extends React.Component {
   render() {
@@ -56,7 +24,7 @@ class App extends React.Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList businesses={this.Business} />
+        <BusinessList businesses={businesses} />
       </div>
     );
   }
